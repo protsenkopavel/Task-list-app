a091436b175aa99f4c14c5a16ad6a2224a40aeea
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-03-22 02:51:23 +0300

    Spring Security, JWT tokens

---
+++ src/main/java/net/protsenko/tasklist/web/security/JwtEntityFactory.java
@@ -0,0 +1,31 @@
+package net.protsenko.tasklist.web.security;
+
+import net.protsenko.tasklist.domain.user.Role;
+import net.protsenko.tasklist.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtEntityFactory {
+
+    public static JwtEntity create(User user) {
+        return new JwtEntity(
+                user.getId(),
+                user.getUserName(),
+                user.getName(),
+                user.getPassword(),
+                mapToGrantedAuthorities(new ArrayList<>(user.getRoles()))
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> roles) {
+        return roles.stream()
+                .map(Enum::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+}
